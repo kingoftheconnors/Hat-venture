3e5637572cad4a63665733452a04e09f28bec7e6
kingoftheconnors <Connormcp777@gmail.com>
2020-10-10 10:59:01 -0400

    Brew explosion (first draft)

--- player/brewing/brewingbottle.gd
+++ player/brewing/brewingbottle.gd
@@ -1,18 +1,22 @@
 extends Node2D
 
 var velo = Vector2()
+var moving = true
 var gravityMultiplier = 0.75
+onready var animator = $AnimationPlayer
 
 func init(direc):
 	velo.x = 80 * direc
 	velo.y = -240
 
 func _physics_process(delta):
-	velo.y += Constants.gravity * gravityMultiplier
-	position += velo * delta
+	if moving:
+		velo.y += Constants.gravity * gravityMultiplier
+		position += velo * delta
 
 func _on_Area2D_body_entered(body):
 	if not body.is_in_group("player"):
 		if body.has_method("damage"):
 			body.damage(false)
-		queue_free()
+		animator.play("explode")
+		moving = false
