309ecf2c5fcbc8833fe52daa598885e7eabbe248
kingoftheconnors <Connormcp777@gmail.com>
2021-01-12 19:24:54 -0500

    Add fake walls

--- common/FakeWalls.gd
+++ common/FakeWalls.gd
@@ -6,8 +6,6 @@
 # to get the required signals called and connected
 extends TileMap
 
-# Final value of the process's tween
-var goal_opacity = 1
 # Rate of change of the tilemap's transparency
 const VISIBILITY_RATE = 0.07
 
@@ -15,14 +13,14 @@ const VISIBILITY_RATE = 0.07
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("player"):
 		tween.stop_all()
-		tween.interpolate_property(self, "modulate", self.modulate, Color(1, 1, 1, 0.5), 0.25, Tween.TRANS_CUBIC)
+		tween.interpolate_property(self, "modulate", self.modulate, Color(1, 1, 1, 0.25), 0.5, Tween.TRANS_CUBIC)
 		tween.start()
 
 ## Signal for making the tilemap visible
 func _on_Area2D_body_exited(body):
 	if body.is_in_group("player"):
 		tween.stop_all()
-		tween.interpolate_property(self, "modulate", self.modulate, Color(1, 1, 1, 1), 0.25, Tween.TRANS_CUBIC)
+		tween.interpolate_property(self, "modulate", self.modulate, Color(1, 1, 1, 1), 0.5, Tween.TRANS_CUBIC)
 		tween.start()
 
 onready var tween : Tween = $Tween
