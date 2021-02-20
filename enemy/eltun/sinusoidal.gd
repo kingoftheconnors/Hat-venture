77dce0486f295c631ec4edde5d3c3aacd914dd03
kingoftheconnors <Connormcp777@gmail.com>
2021-02-19 19:57:36 -0500

    Add Sinuosoidal pace to Eltuns

---
+++ enemy/eltun/sinusoidal.gd
@@ -0,0 +1,39 @@
+extends "res://enemy/Scripts/EnemyController.gd"
+class_name sinusoidal
+
+var velo = Vector2(40, 0)
+## Initial direction. Can be left, stationary, or right
+export(int, -1, 1) var direction : int = 1
+# Enemy's runtime direction
+var cur_direction = Vector2(1,-1)
+var frame = 0
+
+export(int, 20, 120) var wave_height : int = 50
+export(float, 1, 12) var frequency : float = 3
+
+func _ready():
+	cur_direction.x = direction
+	velo.x = velo.x * cur_direction.x
+
+# Frame process function. Moves body.
+func frame(body : KinematicBody2D, sprite : Sprite, delta):
+	if !frozen:
+		print(velo)
+		var retVelo = body.move_and_slide(velo, Vector2.UP)
+		# Zigzag
+		frame += 1
+		velo.y = -wave_height*cos(frequency*frame*PI/180)
+		# Turn around
+		if body.is_on_wall():
+			cur_direction.x = -cur_direction.x
+			velo = Vector2(-velo.x, velo.y)
+		# Turn around sprite of enemies walking backwards
+		if(velo.x * sprite.scale.x < 0):
+			sprite.scale = Vector2(-sprite.scale.x, sprite.scale.y)
+			if sprite.scale.x > 0:
+				cur_direction.x = 1
+			else:
+				cur_direction.x = -1
+
+func get_direction() -> Vector2:
+	return cur_direction
