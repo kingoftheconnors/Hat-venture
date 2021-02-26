115419c082ece4652768589e2c3f05f7e4c13da1
kingoftheconnors <Connormcp777@gmail.com>
2021-02-25 22:13:55 -0500

    Fix optionsMenu horizontal slider's sfx

--- Music/startSong.gd
+++ Music/startSong.gd
@@ -1,12 +1,15 @@
 # Song-starting functionality for using signals to affect music
 extends AudioStreamPlayer
 
-func start_music():
+# Placeholder_arg allows this function to be called by signals
+# with a varying number of arguments, since non of the inputs
+# are used.
+func start_music(_placeholder_arg = null):
 	play()
-func start_sound():
+func start_sound(_placeholder_arg = null):
 	play()
 
-func stop_music():
+func stop_music(_placeholder_arg = null):
 	stop()
-func stop_sound():
+func stop_sound(_placeholder_arg = null):
 	stop()
