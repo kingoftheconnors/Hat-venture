f13b32c262846e09a4a85138851700c16cc6e691
kingoftheconnors <Connormcp777@gmail.com>
2021-04-24 19:09:40 -0400

    Resize window to fit gameboy proportions

--- constants/GameConstants.gd
+++ constants/GameConstants.gd
@@ -3,7 +3,7 @@ extends Node2D
 # Max vertical velocity player can fall at
 var terminalVelocity = 400
 # Size of camera
-var camera_radius = Vector2(160, 120)
+var camera_radius = Vector2(160, 144)
 
 # When set to true, gives players GOD POWERS (keybinds visible
 # from options menu [press escape in game to see])
