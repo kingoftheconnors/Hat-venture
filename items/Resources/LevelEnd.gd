07ac98f67939c70699761a6e3268a54904458929
combozone <play@combo.zone>
2021-05-03 10:43:10 -0400

    Renamed level1 folder to world1

--- items/Resources/LevelEnd.gd
+++ items/Resources/LevelEnd.gd
@@ -2,7 +2,7 @@
 # to the level select
 extends Node2D
 
-export(String) var next_level = "res://level1/level1.tscn"
+export(String) var next_level = "res://world1/level1.tscn"
 
 func end_level():
 	PlayerGameManager.start_level(next_level)
--- levelgameover/Continue.gd
+++ levelgameover/Continue.gd
@@ -11,7 +11,7 @@ func _unhandled_input(event):
 	# Change keybind
 	if has_focus() and event.is_action_pressed("ui_A"):
 		PlayerGameManager.reset_stats()
-		PlayerGameManager.start_level("res://level1/level1.tscn")
+		PlayerGameManager.start_level("res://world1/level1.tscn")
 
 onready var cursor : Control = $Selector
 export(bool) var default_focus = false
