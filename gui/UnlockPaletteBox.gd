32e1ae356d88deff35e5a1b8e9ec9d95116772a3
kingoftheconnors <Connormcp777@gmail.com>
2021-11-16 00:06:57 -0500

    Add storybook page

--- gui/UnlockPaletteBox.gd
+++ gui/UnlockPaletteBox.gd
@@ -1,5 +1,12 @@
 extends NinePatchRect
 
 func show_palette_get(palette_name):
+	$Dialog.text = "New Palette Unlocked"
 	$PaletteName.text = palette_name
+	$Dialog2.text = "Added to the pause menu"
+	$AnimationTree['parameters/playback'].travel("enter")
+func show_storybook_get(num_pages : int):
+	$Dialog.text = "New Storybook Page Found"
+	$PaletteName.text = str(num_pages) + "/0"
+	$Dialog2.text = "There is no Gallery... YET"
 	$AnimationTree['parameters/playback'].travel("enter")
