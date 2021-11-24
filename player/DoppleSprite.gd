b4cda647bbb8e81e1881d2dd49ee5461e4d46dbe
kingoftheconnors <Connormcp777@gmail.com>
2021-11-23 21:44:30 -0500

    Fix player flash affecting all player-shaded nodes

---
+++ player/DoppleSprite.gd
@@ -0,0 +1,10 @@
+extends Sprite
+
+export(NodePath) var copy_target = "."
+onready var copy : Sprite = get_node(copy_target)
+
+func _process(delta):
+	texture = copy.texture
+	frame = copy.frame
+	hframes = copy.hframes
+	vframes = copy.vframes
