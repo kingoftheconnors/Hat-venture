aca2130caa6670480beb27b977b6be88c5380f60
kingoftheconnors <Connormcp777@gmail.com>
2022-02-15 18:02:18 -0500

    Add failsafe for palette index being too high when starting game

--- gui/optionsmenu/setting_logic/palette_selector.gd
+++ gui/optionsmenu/setting_logic/palette_selector.gd
@@ -6,4 +6,7 @@ func main(value: Dictionary) -> void:
 	if Constants.DEBUG_MODE:
 		Gui.set_palette(Palettes.palettes.keys()[value["value"]])
 	else:
+		var palettes = SaveSystem.access_data().get_palettes()
+		if value["value"] >= palettes.size():
+			value["value"] = 0
 		Gui.set_palette(SaveSystem.access_data().get_palettes()[value["value"]])
