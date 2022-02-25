a44217e379333b2fc4532cf3fbe965ea9cfb4323
kingoftheconnors <Connormcp777@gmail.com>
2022-02-24 21:20:57 -0500

    Add new Hat Kid dance for new music

--- items/timePiece.gd
+++ items/timePiece.gd
@@ -3,7 +3,9 @@ class_name timePiece
 func collect_as_item(body, this):
 	# Play Animation
 	PlayerGameManager.pause_except([body])
+	SoundSystem.fadeout_music()
 	this.get_node("AnimationTree")["parameters/playback"].travel("Collect")
+	body.walk_to_node(this)
 	body.animate("dance")
 	return false
 
